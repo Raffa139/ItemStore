d81f3b35d55fc0b4820e637923e95484b552b482
Raffael Engelbart <raffael.engelbart@web.de>
2022-06-22 21:40:55 +0200

    Project setup

---
+++ src/main/java/com/re/bi/itemstore/ItemStoreApplication.java
@@ -0,0 +1,11 @@
+package com.re.bi.itemstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemStoreApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ItemStoreApplication.class, args);
+  }
+}
---
+++ src/test/java/com/re/bi/itemstore/ItemStoreApplicationTest.java
@@ -0,0 +1,11 @@
+package com.re.bi.itemstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemStoreApplicationTest {
+  @Test
+  void contextLoads() {
+  }
+}

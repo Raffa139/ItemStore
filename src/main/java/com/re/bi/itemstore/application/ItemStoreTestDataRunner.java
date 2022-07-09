e9a6c772f6d33c720b0bb77b9403d8fda533614d
Raffael Engelbart <raffael.engelbart@web.de>
2022-07-09 10:50:22 +0200

    Creation of test data can be disabled by env-variable, remove test

--- src/main/java/com/re/bi/itemstore/application/ItemStoreTestDataRunner.java
+++ src/main/java/com/re/bi/itemstore/application/ItemStoreTestDataRunner.java
@@ -5,6 +5,7 @@ import com.re.bi.itemstore.domain.item.ItemRepository;
 import com.re.bi.itemstore.domain.item.ItemTags;
 import com.re.bi.itemstore.domain.item.ItemValue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -18,15 +19,20 @@ public class ItemStoreTestDataRunner implements CommandLineRunner {
   @Autowired
   private ItemRepository itemRepository;
 
+  @Value("${sys.items.test-data:true}")
+  private boolean createTestData;
+
   @Override
   public void run(String... args) {
-    Set<String> tagSetOne = Set.of("ABC", "XYZ", "123");
-    Set<String> tagSetTwo = Set.of("FFF", "456", "REN");
-    Random random = new Random();
-    List<Item> items = new ArrayList<>();
-    for (int i = 0; i < 10; i++) {
-      items.add(new Item(new ItemValue(i*10+100), new ItemTags(random.nextFloat() > 0.5f ? tagSetOne : tagSetTwo)));
+    if (createTestData) {
+      Set<String> tagSetOne = Set.of("ABC", "XYZ", "123");
+      Set<String> tagSetTwo = Set.of("FFF", "456", "REN");
+      Random random = new Random();
+      List<Item> items = new ArrayList<>();
+      for (int i = 0; i < 10; i++) {
+        items.add(new Item(new ItemValue(i * 10 + 100), new ItemTags(random.nextFloat() > 0.5f ? tagSetOne : tagSetTwo)));
+      }
+      itemRepository.saveAll(items);
     }
-    itemRepository.saveAll(items);
   }
 }
